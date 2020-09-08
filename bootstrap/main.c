bd2819e9edf2ee47a2e405b54cc9a093c7acac91
Tobe O <thosakwe@gmail.com>
2020-09-08 08:30:35 -0400

    Build bootstrap.bin

--- bootstrap/main.c
+++ bootstrap/main.c
@@ -2,19 +2,20 @@
 #include "../third_party/printf/printf.h"
 #define COM1 0x3f8
 
-void _putchar(char ch) {
-  osdev_outb(COM1, ch);
-}
+void _putchar(char ch) { osdev_outb(COM1, ch); }
 
 void osdev_bootstrap_main(unsigned long magic, unsigned long addr) {
   if (magic != MULTIBOOT_BOOTLOADER_MAGIC) {
     // TODO: Log improper magic
+    printf("Invalid magic.\n");
     return;
   }
 
-	// Search for a kernel module. If we find one, then load it.
-	
-	// Create the appropriate kind of page table.
-	// Load the kernel as an ELF binary, and then map every page
-	// into memory.
+  printf("WE MADE IT\n");
+
+  // Search for a kernel module. If we find one, then load it.
+
+  // Create the appropriate kind of page table.
+  // Load the kernel as an ELF binary, and then map every page
+  // into memory.
 }
